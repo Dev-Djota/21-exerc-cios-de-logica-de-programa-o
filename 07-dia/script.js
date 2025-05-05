7de9eaa00877e6cc0a41264f91afc6741e2da973
Danillo Jesus <danilllog366@gmail.com>
2025-05-05 17:11:05 -0300

    contador exercicio

---
+++ 07-dia/script.js
@@ -0,0 +1,17 @@
+// 1 - Faça um algoritmo que receba um valor, inteiro e positivo, e usando a estrutura FOR, mostre no console uma contagem de 0 até o número recebido.
+
+
+// 2 - Desenvolva um algoritmo que usando a estrutura FOR mostre no console uma contagem de 0 a 50, porém, essa contagem tem que ser feita de 5 em 5.
+//    Ex.: 0 5 10 15 20 25 … 45 50 Fim!
+
+
+
+// 3 - Agora faça com que o algoritmo do exercício anterior exiba a mesma contagem mas na ordem inversa.
+//    Ex.: 50 45 40 35 30 25 … 5 0 Fim!
+
+ let contador = parseInt(prompt("digite um número:"))
+
+ for(i = 0; i <= contador; i++){
+     console.log(i)
+
+ }
