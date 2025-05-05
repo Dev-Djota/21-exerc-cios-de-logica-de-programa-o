1a7e5acb5cc032c83be189f6f1534d241d63be05
Danillo Jesus <danilllog366@gmail.com>
2025-05-05 17:15:12 -0300

    contador exercicio 2

---
+++ 07-dia/script2.js
@@ -0,0 +1,14 @@
+// 1 - Faça um algoritmo que receba um valor, inteiro e positivo, e usando a estrutura FOR, mostre no console uma contagem de 0 até o número recebido.
+
+
+// 2 - Desenvolva um algoritmo que usando a estrutura FOR mostre no console uma contagem de 0 a 50, porém, essa contagem tem que ser feita de 5 em 5.
+//    Ex.: 0 5 10 15 20 25 … 45 50 Fim!
+
+
+
+// 3 - Agora faça com que o algoritmo do exercício anterior exiba a mesma contagem mas na ordem inversa.
+//    Ex.: 50 45 40 35 30 25 … 5 0 Fim!
+
+for(i = 0; i <= 50 ; i += 5){
+    console.log(i)
+}
